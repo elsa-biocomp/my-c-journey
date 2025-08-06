65cf6351bdd754b063e261a4900179a81a371b33
Elsa Mukherjee <224131383+elsa-biocomp@users.noreply.github.com>
2025-08-06 17:24:09 +0530

    calculating circumference of a circle

---
+++ circumference.c
@@ -0,0 +1,13 @@
+// calculation of the circumference of a circle
+// author: elma ; date : 6/8/25
+#include<stdio.h>
+
+int main() {
+    float pi , c ; // pi = pi of a circle 3.14 , C = circumference
+    int r; // r= radius of a circle 
+    pi= 3.14;
+    r= 7;
+    c= 2*pi*r; // formula to calculate circumference 
+    printf(" the circumference of the circle is : %f", c);
+
+}
